cff62e153a8b54db1e7ac486c119ecc4fe993c99
Dolapo Onifade <69683168+Oluwadolapor@users.noreply.github.com>
2022-07-12 02:53:31 +0000

    check 1

--- src/assignment.js
+++ src/assignment.js
@@ -17,9 +17,9 @@ function sumOfNumbers(arrayOfNumbers) {
   let sum = 0;
   for (let index = 0; index < arrayOfNumbers.length;    index++) {
     sum += arrayOfNumbers[index]
-  
   }
   return sum
+  console.log(arrayOfNumbers)
 }
 assignment.sumOfNumbers = sumOfNumbers
 
